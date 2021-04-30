c013bb92d7a30d9aa85962f7217494c45c8060a6
Adrian Barros <adriansbarros1@gmail.com>
2021-04-30 00:46:09 -0400

    style intro page

--- portafolio/src/App.js
+++ portafolio/src/App.js
@@ -1,11 +1,16 @@
 
 import React from "react";
-
+//import pages
 import AboutMe from './pages/AboutMe'
+//import style
+import GlobalStyle from "./components/GlobalStyle";
+
+
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutMe />
     </div>
   );
--- portafolio/src/components/About.js
+++ portafolio/src/components/About.js
@@ -1,26 +1,60 @@
 import React from "react";
 import Intro_img from '../img/Intro_img.JPG'
-
+import styled from 'styled-components'
 const AboutSection = () => {
     return (
-        <div>
-            <div className="description">
+        <StyleAbout>
+            <StyledDescription>
                 <div className="title">
-                    <div className="hide">
+                    <StyledHide>
                         <h2>Hello There!</h2>
-                    </div>
-                    <div className="hide">
-                        <h2>My name is Adrian Barros</h2>
-                    </div>
+                    </StyledHide>
+                    <StyledHide>
+                        <h2>My name is <span>Adrian Barros</span></h2>
+                    </StyledHide>
                     <p>I am a reacent graduate of Computer Science looking for a job</p>
                     <button>contact me</button>
                 </div>
-                <img src={Intro_img} alt="picture of me" />
-            </div>
 
-        </div>
+            </StyledDescription>
+            <StyledImg>
+                <img src={Intro_img} alt="picture of me" />
+            </StyledImg>
+        </StyleAbout>
 
     )
 }
+
+//styled components
+const StyleAbout = styled.div`
+    min-height: 90vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5rem 10rem;
+    color: white;
+`
+const StyledDescription = styled.div`
+    flex: 1;
+    padding-right: 5rem;
+    h2{
+        font-weight: lighter;
+    }
+
+
+`
+const StyledImg = styled.div`
+    flex: 1;
+    img{
+        width: 100%;
+        height: 80vh;
+        object-fit: cover;
+    }
+
+`
+const StyledHide = styled.div`
+    overflow: hidden;
+
+`
 
 export default AboutSection;
